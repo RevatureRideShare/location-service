a03641cf3be6dc4bbc9d3292693dc519a0a9bbe8
unknown <mcbride.mik@gmail.com>
2019-12-02 12:08:49 -0500

    Removing @CrossOrigin annotations from the controllers.

--- src/main/java/com/revature/controller/TrainingLocationController.java
+++ src/main/java/com/revature/controller/TrainingLocationController.java
@@ -16,7 +16,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -30,7 +29,6 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Roberto Rodriguez
  */
 @RestController
-@CrossOrigin
 public class TrainingLocationController {
   private TrainingLocationService trainingLocationService;
 
